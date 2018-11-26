4536278a6489294165960f93d6df9a3e7c04b8a6
JerryGor <515252309jerry@gmail.com>
2018-11-26 17:11:34 -0500

    First attempt of detecting license plate using openCV with figure study but failed. Accuracy was poor due to improper use of threshold and ratios. Need to learn more on enrode findcontour and OTSU methods.

--- lp_detect.py
+++ lp_detect.py
@@ -2,13 +2,18 @@ import cv2
 import numpy as np
 import imutils
 
-def detectPlates(car_image):
+minPlateW=80
+minPlateH=40
+
+def detect_plate_region(car_image):
     # Since license plate have great contrast between plate background
     # and plate characters, we perform Black Hat operation on gray scale
     # aka the difference between source image and Closing operation in order
     # to enhance image
 
     img = cv2.imread(car_image)
+    if img.shape[1] > 640:
+        img = imutils.resize(img, width=640)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rectangle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 5))
     black_hat = cv2.morphologyEx(gray_img, cv2.MORPH_BLACKHAT, rectangle_kernel)
@@ -23,24 +28,100 @@ def detectPlates(car_image):
     cv2.waitKey(0)
 
     # Binary threshold
-    ret, threshold = cv2.threshold(closing, 50, 255, cv2.THRESH_BINARY)
+    ret, light_mask = cv2.threshold(closing, 50, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow("binary threshold", threshold)
+    cv2.imshow("binary threshold", light_mask)
     cv2.waitKey(0)
 
     # Sobel
     ddepth = cv2.CV_32F
     sobel_x = cv2.Sobel(black_hat, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-    cv2.imshow("sobel x direction", sobel_x)
+    sobel_x_abs = np.absolute(sobel_x)
+    (min_val, max_val) = (np.min(sobel_x_abs), np.max(sobel_x_abs))
+    sobel_x_scale = (255 * ((sobel_x_abs - min_val) / (max_val - min_val))).astype("uint8")
+    cv2.imshow("sobel x-direction scale", sobel_x_scale)
+    cv2.waitKey(0)
+
+    # Gaussian Filter
+    sobel_x_gaussian = cv2.GaussianBlur(sobel_x_scale, (5, 5), 0)
+    # Perform Closing operation (Dilation followed by Erosion)
+    sobel_x_closing = cv2.morphologyEx(sobel_x_gaussian, cv2.MORPH_CLOSE, rectangle_kernel)
+    cv2.imshow("sobel x-direction closing", sobel_x_closing)
+    cv2.waitKey(0)
+    # Perform Otsu thresholding
+    ret, otsu_thresh = cv2.threshold(sobel_x_closing, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    cv2.imshow("OTSU threshold", otsu_thresh)
+    cv2.waitKey(0)
+
+    # Clean up image
+    thresh = cv2.erode(otsu_thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cv2.imshow("Clean up: Closing", otsu_thresh)
+    cv2.waitKey(0)
+    thresh = cv2.bitwise_and(thresh, thresh, mask=light_mask)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    thresh = cv2.erode(thresh, None, iterations=1)
+    cv2.imshow("Clean up: mask", thresh)
     cv2.waitKey(0)
 
 
+    # Contour
+    regions = []
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+    # clone = img.copy()
+    # cv2.drawContours(clone, cnts, -1, (0, 255, 0), 2)
+    #
+    # cv2.imshow("Contour", clone)
+    # cv2.waitKey(0)
+
+    # loop over the contours
+    for c in cnts:
+        # grab the bounding box associated with the contour and compute the area and
+        # aspect ratio
+        (w, h) = cv2.boundingRect(c)[2:]
+        aspectRatio = w / float(h)
+
+        # print("aspectRatio: " + str(aspectRatio))
+        # print("w: " + str(w))
+        # print("h: " + str(h))
+        #
+        # clone = img.copy()
+        # cv2.drawContours(clone, c, -1, (0, 255, 0), 2)
+        #
+        # cv2.imshow("Contour", clone)
+        # cv2.waitKey(0)
+
+
+        # compute the rotated bounding box of the region
+        rect = cv2.minAreaRect(c)
+        box = np.int0(cv2.cv.BoxPoints(rect)) if imutils.is_cv2() else cv2.boxPoints(rect)
+
+        # ensure the aspect ratio, width, and height of the bounding box fall within
+        # tolerable limits, then update the list of license plate regions
+        if (aspectRatio > 2 and aspectRatio < 6) and h > minPlateH and w > minPlateW:
+            print("added")
+            regions.append(box)
+
+    return regions
 
 
 
+def detect_plate(car_image):
+    img = cv2.imread(car_image)
+    if img.shape[1] > 640:
+        img = imutils.resize(img, width=640)
+    regions = detect_plate_region(car_image)
+    # regions = detect_origin(car_image)
+    print(len(regions))
 
+    for region in regions:
+        lpBox = np.array(region).reshape((-1, 1, 2)).astype(np.int32)
+        cv2.drawContours(img, [lpBox], -1, (0, 255, 0), 2)
 
-
+    cv2.imshow("Detection", img)
+    cv2.waitKey(0)
 
 
 
@@ -59,7 +140,10 @@ if __name__ == "__main__":
     car9 = "cars/car9.jpg"
     car10 = "cars/car10.jpg"
 
-    detectPlates(car2)
+    sample1 = "cars/sample1.png"
+    sample2 = "cars/sample2.png"
+
+    detect_plate(car1)
 
 
 
